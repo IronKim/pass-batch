b9434bbe142fe1993b3e970497b083d68d5d31b8
IronKim <tankokim@gmail.com>
2024-02-22 19:25:22 +0900

    first commit

---
+++ src/test/java/com/fs/pass/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
