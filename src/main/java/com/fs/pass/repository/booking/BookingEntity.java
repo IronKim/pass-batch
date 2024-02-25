9c9668011821f9663337372335938bd4b6fadd8d
IronKim <tankokim@gmail.com>
2024-02-26 00:38:20 +0900

    예약과 알람 Entity 및 Repository 추가
    예약 상태와 알람이벤트(어떤 알람인지) Enum도 함께 추가

---
+++ src/main/java/com/fs/pass/repository/booking/BookingEntity.java
@@ -0,0 +1,37 @@
+package com.fs.pass.repository.booking;
+
+import com.fs.pass.repository.BaseEntity;
+import com.fs.pass.repository.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "booking")
+public class BookingEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
+    private Integer bookingSeq;
+    private Integer passSeq;
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+    private boolean usedPass;
+    private boolean attended;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private LocalDateTime cancelledAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private UserEntity userEntity;
+
+}
---
+++ src/main/java/com/fs/pass/repository/booking/BookingStatus.java
@@ -0,0 +1,5 @@
+package com.fs.pass.repository.booking;
+
+public enum BookingStatus {
+    READY, PROGRESSED, COMPLETED, CANCELLED
+}
