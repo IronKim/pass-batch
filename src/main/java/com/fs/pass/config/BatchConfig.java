78006ec86a34b539a907786e951b9cde376f81de
IronKim <tankokim@gmail.com>
2024-02-27 22:12:54 +0900

    REST API를 호출하여 Job을 실행시키기
    먼저 동시에 서버를 띄우기 위해 port번호를 변경해주었고 배치작업이 자동으로 실행되지 않게 해주었다.
    
    BatchConfig를 통해 잡 레지스트리 관련 설정을 해주었다.
    
    JobLauncherController를 통해 API를 호출한다.

--- src/main/java/com/fs/pass/config/BatchConfig.java
+++ src/main/java/com/fs/pass/config/BatchConfig.java
@@ -2,6 +2,9 @@ package com.fs.pass.config;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -13,31 +16,19 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 /* * @EnableBatchProcessing * Spring Batch 기능을 활성화하고 배치 작업을 설정하기 위한 기본 구성을 제공합니다. * JobBuilderFactory, StepBuilderFactory를 빈으로 등록되어 Job, Step 구현 시 사용할 수 있습니다. */
 
+@EnableBatchProcessing
 @Configuration
 public class BatchConfig {
 
+    /**
+     * JobRegistry는 context에서 Job을 추적할 때 유용합니다.
+     * JobRegistryBeanPostProcessor는 Application Context가 올라가면서 bean 등록 시, 자동으로 JobRegistry에 Job을 등록 시켜줍니다.
+     */
     @Bean
-    public Job passJob(JobRepository jobRepository, Step passStep) {
-        return new JobBuilder("passJob", jobRepository)
-                .start(passStep)
-                .build();
-    }
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+        JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+        return jobRegistryBeanPostProcessor;
 
-    @Bean
-    public Step passStep(final JobRepository jobRepository,
-                         final Tasklet passTasklet,
-                         final PlatformTransactionManager platformTransactionManager) {
-        return new StepBuilder("passStep", jobRepository)
-                .tasklet(passTasklet, platformTransactionManager)
-                .build();
     }
-
-    @Bean
-    public Tasklet passTasklet() {
-        return ((contribution, chunkContext) -> {
-            System.out.println("Execute PassStep");
-            return RepeatStatus.FINISHED;
-        });
-    }
-
 }
